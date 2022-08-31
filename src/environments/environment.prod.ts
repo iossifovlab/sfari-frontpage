61760782a6add4276bbed9e2547df90115d60d72
iordanivanov <iiordan.ivanov@gmail.com>
2022-08-31 16:21:55 +0300

    GPF-1793 More testing of production settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: true,
   instances: {
     hg19: {
-      apiPath: 'http://localhost:8000/gpf_prefix/api/v3',
-      frontendPath: 'http://localhost:8000/gpf_prefix',
+      apiPath: 'http://gpf:8000/gpf_prefix/api/v3',
+      frontendPath: 'http://gpf:8000/gpf_prefix',
     },
   },
-  authPath: 'http://localhost:8000/gpf_prefix',
+  authPath: 'http://gpf:8000/gpf_prefix',
   oauthClientId: 'gpfjs-frontpage'
 };
