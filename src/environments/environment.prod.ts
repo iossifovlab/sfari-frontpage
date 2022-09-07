44160889c6ca588e689e777f096bf7c6aa1ef87d
iordanivanov <iiordan.ivanov@gmail.com>
2022-09-07 15:48:23 +0300

    index.html, environment.prod.ts: add support for frontpage_prefix, require explicitly environment.ts.prod to be filled

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,18 @@
+
 export const environment = {
   production: true,
+  oauthClientId: 'gpfjs-frontpage',
   instances: {
-    hg19: {
-      apiPath: 'http://localhost:8000/gpf_prefix/api/v3',
-      frontendPath: 'http://localhost:8000/gpf_prefix',
-    },
+    // Must be filled by some script !
+
+    // Example:
+    //    hg19: {
+    //      apiPath: 'http://localhost:9000/gpf_prefix/api/v3',
+    //      frontendPath: 'http://localhost:9000/gpf_prefix',
+    //    },
   },
-  authPath: 'http://localhost:8000/gpf_prefix',
-  oauthClientId: 'gpfjs-frontpage'
+  authPath: ''
+  // Must be a path to a gpf instance with an oauth application
+  // Example: (the first instance in the above example)
+  // authPath: 'http://localhost:9000/gpf_prefix',
 };
